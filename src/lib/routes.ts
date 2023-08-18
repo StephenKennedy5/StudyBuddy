a04c53f34eda977f173bced01a0bd006fbef066b
StephenKennedy5 <s5kenned@ucsd.edu>
2023-08-17 18:34:45 -0700

    Get study sessions api endpoint

---
+++ src/lib/routes.ts
@@ -0,0 +1,23 @@
+/* 
+
+    Create Routes that will be used in API Calls to gather info from the database
+
+    Get Study Sessions 
+        Takes
+            - Has UserID
+        Return
+            - Array of Dics of Study Sessions for particular User
+            [
+                {
+                    StudySession: Text
+                    Subject: Text
+                    chat_log_id: UUID
+                },
+                {
+                    StudySession: Text
+                    Subject: Text
+                    chat_log_id: UUID
+                }
+            ]
+
+*/
