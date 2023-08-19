3f5474727bb56e44ffad10cc46260f2c13a92cbb
StephenKennedy5 <s5kenned@ucsd.edu>
2023-08-18 18:24:24 -0700

    New message API endpoint

--- src/pages/api/[userid]/study-session/[studySessionId]/newchatmessage.ts
+++ src/pages/api/[userid]/study-session/[studySessionId]/newchatmessage.ts
@@ -4,7 +4,10 @@ import knex from '../../../../../../database/knex';
 // Chat logs ID = 80d2fb15-0df8-44b6-b389-2e817f7b82b5;
 // study session ID = 8f2d1990-fd62-4171-bc9f-0bd435552a2c;
 
-/* Return an array of chat messages ordered by creation_date */
+/* Add a new chat message to the chat log */
+// Need create new chat message to insert with user_id, chat_logs_id, creation_Date, message
+
+const uuid = require('uuid');
 
 export default async function GetStudySessions(
   req: NextApiRequest,
@@ -13,15 +16,24 @@ export default async function GetStudySessions(
   try {
     const userId = req.query.userid;
     const studySessionId = req.query.studySessionId;
+    const { chat_message } = req.body;
     const { chat_log_id } = await knex('study_sessions')
       .select()
-      .where('user_id', userId)
-      .andWhere('id', studySessionId)
+      .where('id', studySessionId)
       .first();
+
+    const new_chat_message = await knex('chat_messages').insert({
+      id: uuid.v4(),
+      chat_id: chat_log_id,
+      chat_message: chat_message,
+      user_id: userId,
+    });
+
     const chat_messages = await knex('chat_messages')
       .select()
       .where('chat_id', chat_log_id)
       .orderBy('creation_date');
+
     res.status(200).json(chat_messages);
   } catch (error) {
     console.error('Error:', error);
--- src/pages/api/[userid]/study-session/chatmessages.ts
+++ src/pages/api/[userid]/study-session/chatmessages.ts
@@ -2,10 +2,12 @@
 
 API Endpoint returns all chat messages for a particular study session
 
-Get All Chat messages for particular Study Sessin
+Get All Chat messages for particular Study Session
+
+an api endpoint that adds a new chat message to the chat logs
 
 api/{userId}/study-sessions/{studySessionId}/chatLogs
 
-Need to add userId to chat messages
-need to add chat log Id to Study Session
+Need to add userId to chat messages 
+need to add chat log Id to Study Session 
 */
