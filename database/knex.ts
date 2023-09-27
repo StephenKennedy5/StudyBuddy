9966a9dc64cea963a3eccc81d9fd29ca021de96b
StephenKennedy5 <s5kenned@ucsd.edu>
2023-09-26 17:58:22 -0700

    potential fix to to many psql clients

--- database/knex.ts
+++ database/knex.ts
@@ -1,21 +1,25 @@
-import knexFactory from 'knex';
+import knexFactory, { Knex } from 'knex';
 
+import * as knexfile from '../knexfile';
 import config from '../knexfile';
 
+// type KnexConfig = {
+//   development: {
+//     client: string;
+//     connection: {
+//       host: string;
+//       database: string;
+//       user: string;
+//       password: string;
+//     };
+//     migrations: {
+//       tableName: string;
+//       directory: string;
+//     };
+//   };
+// };
 type KnexConfig = {
-  development: {
-    client: string;
-    connection: {
-      host: string;
-      database: string;
-      user: string;
-      password: string;
-    };
-    migrations: {
-      tableName: string;
-      directory: string;
-    };
-  };
+  development: Knex.Config;
 };
 
 const environment = process.env.NODE_ENV || 'development';
--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,38 @@ module.exports = {
       user: 'stephen',
       password: 'buddydb',
     },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './database/migrations',
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      // Production database connection details
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './database/migrations',
+    },
+  },
+  test: {
+    client: 'pg',
+    connection: {
+      // Test database connection details
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
     migrations: {
       tableName: 'knex_migrations',
       directory: './database/migrations',
